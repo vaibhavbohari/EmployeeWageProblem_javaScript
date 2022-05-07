defe243aae5420abb043955d7c60d6b38980fcce
vaibhavbohari <vaibhavbohari@gmail.com>
2022-05-07 23:16:21 +0530

    Check Employee is Present Or Absent

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,17 @@
-console.log("Welcome to employee wage problem");
+// UC1 Check Employee Presence
+/// Variable
+const IS_ABSENT = 0;
+/// Generates 0 or 1 randomly.
+let empcheck = Math.floor(Math.random() * 10) % 2;
+/// Checks Employee is present or not.
+if(empcheck == IS_ABSENT)
+{
+    // Prints Employee is absent when random number is 0.
+    console.log("Employee is Absent.");
+    
+}
+else
+{
+    // Prints Employee is Present when random number is 1.
+    console.log("Employee is Present.");
+}
